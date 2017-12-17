15e332e4014e34711b10e864cb7ee17c1429c96b
Simon <simonxd@gmail.com>
2017-12-17 20:41:23 -0200

    added microphone to waveform demo

--- demo/tone-ui-waveform.js
+++ demo/tone-ui-waveform.js
@@ -1,17 +1,30 @@
 const Tone = require('tone')
 const Meta = require('../')
 
-let noteContainer = document.querySelector('#notes')
-let noteButtons = Array.from(document.querySelectorAll('[data-note]'))
-
-let synth = new Tone.Synth()
-
 let waveformUI = Meta.Tone.ui.waveform({
 	canvas: document.querySelector('#waveform-canvas'),
 	canvasWidth: window.innerWidth,
 	canvasHeight: window.innerHeight,
+
+	size: 512,
+
+	render: {
+		fillStyle: 'black',
+		strokeStyle: 'black',
+	},
 })
+
+let synth = new Tone.Synth()
 synth.fan(Tone.Master, waveformUI.audioNode)
+
+let microphone = new Tone.UserMedia();
+
+/**
+ * UI
+ */
+let noteContainer = document.querySelector('#notes')
+let noteButtons = Array.from(document.querySelectorAll('[data-note]'))
+let microphoneToggle = document.querySelector('#microphone-toggle')
 
 noteButtons.forEach(element => {
 	let note = element.getAttribute('data-note')
@@ -21,4 +34,15 @@ noteButtons.forEach(element => {
 
 document.documentElement.addEventListener('mouseup', e => {
 	synth.triggerRelease()
+})
+
+microphoneToggle.addEventListener('change', (e) => {
+	if (microphoneToggle.checked) {
+		// opening the input asks the user to activate their mic
+		microphone.open().then(function(){
+			microphone.connect(waveformUI.audioNode)
+		})
+	} else {
+		microphone.disconnect(waveformUI.audioNode)
+	}
 })
